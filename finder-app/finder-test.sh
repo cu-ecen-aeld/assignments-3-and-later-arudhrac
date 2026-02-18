4cda274248fee63828bd5edd6e0611689933ba26
arudhrac <arudhra.c9@gmail.com>
2026-02-18 19:56:30 +0530

    update for assn4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -61,10 +61,10 @@ fi
 for i in $( seq 1 $NUMFILES)
 do
 	#./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(./usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
 #writing the output
 echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
